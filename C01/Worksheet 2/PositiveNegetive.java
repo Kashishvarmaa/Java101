325b62288d74384132aeddde02dd5ed9125ec572
Kashishvarmaa <kashishvarmaa@gmail.com>
2024-03-14 02:32:55 +0530

    first commit

--- C01/Worksheet 2/PositiveNegetive.java
+++ C01/Worksheet 2/PositiveNegetive.java
@@ -2,7 +2,7 @@
 
 import java.util.Scanner;
 public class PositiveNegetive {
-    public static void main(String args[]) {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the number: ");
         int num = sc.nextInt();
