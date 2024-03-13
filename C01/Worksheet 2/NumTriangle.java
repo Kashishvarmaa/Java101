d0f02eb4613224c71f3d82f6a142d11a292a10c2
Kashishvarmaa <kashishvarmaa@gmail.com>
2024-03-14 02:32:25 +0530

    first commit

---
+++ C01/Worksheet 2/NumTriangle.java
@@ -0,0 +1,16 @@
+/*Write a program in Java to make such a pattern like right angle triangle with a number which will repeat a number in a row. The pattern is as follows
+1
+22
+333
+4444 */
+public class NumTriangle {
+    public static void main(String[] args) {
+        int rows = 4;
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+    }
+}
